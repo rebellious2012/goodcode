0dd305f4a4a3910bcbe6e71ad146dad3c23106dd
rebellious2012 <romeo_s@bigmir.net>
2026-01-13 11:17:24 +0100

    new pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'CodElevator - Leading IT Company')</title>
+    <meta name="google-site-verification" content="eEm_jo1DU-jl5GIWEa3wANkWLe-lNPZmm-B_Z1ALcDg" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <script>
